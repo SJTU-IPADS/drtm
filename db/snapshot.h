816a37ecbeee04fe0db9b0bb12a963ec26cc69ff
wxd <wxdwfc@gmail.com>
2015-12-06 18:40:42 +0800

    minor

--- db/snapshot.h
+++ db/snapshot.h
@@ -21,9 +21,6 @@
  *
  */
 
-// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file. See the AUTHORS file for names of contributors.
 
 #ifndef STORAGE_LEVELDB_DB_SNAPSHOT_H_
 #define STORAGE_LEVELDB_DB_SNAPSHOT_H_
