d80d32a7b99e80b593816bc222651dee7d3e84cb
wxd <wxdwfc@gmail.com>
2015-12-06 18:39:13 +0800

    minor

--- db/dbrawtx.cc
+++ db/dbrawtx.cc
@@ -22,10 +22,6 @@
  *
  */
 
-// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file. See the AUTHORS file for names of contributors.
-
 
 #include <string.h>
 #include "dbrawtx.h"
